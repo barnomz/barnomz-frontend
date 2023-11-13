03f03c6b3fc955844b01d3569ea6253751bd1c97
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-11-13 22:01:17 +0330

    Add layout.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import Layout from '../components/layout'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
