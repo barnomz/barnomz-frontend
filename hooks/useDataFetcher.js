f4c9df3cba18b07851e1f385f5f682b0f784e9cb
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-11-13 22:42:14 +0330

    Add swr package and useDataFetcher hook

---
+++ hooks/useDataFetcher.js
@@ -0,0 +1,19 @@
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+const useDataFetcher = (key, fetcher, config = {}) => {
+    return useSWR(key, fetcher, config)
+}
+
+// export const useUser = (id) => {
+//     const {data, error, isLoading} = useDataFetcher(`/api/user/${id}`, fetcher)
+//
+//     return {
+//         user: data,
+//         isLoading,
+//         isError: error
+//     }
+// }
+
+// TODO: Add endpoints
