9595fa63b5a394cd11195aa288798fb4fd70ed81
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-11-17 17:03:49 +0330

    Add color palette to the project and add it to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import { colors } from './palette.js'
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,13 +7,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors,
   },
   plugins: [],
 }
