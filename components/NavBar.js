59b0c6ac12f0cd5fe252e7beb86bd889f9ba6701
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-12-01 22:54:59 +0330

    Add two login and register buttons in NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import logoSvg from '../public/images/barnomz-horizontal-logo.svg'
+import BBtn from '@/components/BBtn'
 
 export default function NavBar() {
   return (
     <div className="px-[1.5rem] py-[0.75rem] h-[3.75rem] w-full flex justify-between items-center bg-primary/50 backdrop-blur">
       <Image src={logoSvg} alt="Barnomz Logo" priority />
+      <div className='flex gap-4'>
+        <BBtn to='/auth/login' color='primary' className='h-[2.25rem]'>
+          ورود
+        </BBtn>
+        <BBtn to='/auth/register' className='h-[2.25rem]'>
+          ثبت‌نام
+        </BBtn>
+      </div>
     </div>
   )
 }
