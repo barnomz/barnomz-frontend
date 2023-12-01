cc402976e13286828d2b6a5db04a64e14629ac7a
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-12-01 22:54:40 +0330

    Add config of fontAwesome in layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,7 @@
 import NavBar from '@/components/NavBar'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 export default function Layout({ children }) {
   return (
