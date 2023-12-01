4eedfe289479caaa285eddb260b22303c92f01f1
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-12-01 21:35:40 +0330

    Remove comments and extra main tag from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,11 @@ import '@/styles/globals.css'
 import Layout from '@/components/layout'
 import { SessionProvider } from 'next-auth/react'
 
-// export const metadata = {
-//   title: "صرافی ارز دیجیتال تبدیل | بهترین صرافی آنلاین ایرانی",
-//   description:
-//     "تبدیل، بهترین صرافی ارز دیجیتال ایرانی، تمام نیازهای شما از یک صرافی رمز ارز را تأمین می‌کند. نهایت امنیت را در سایت صرافی تبدیل تجربه کنید.",
-//   icons: {
-//     icon: "/favicon.ico",
-//   },
-// };
-
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Layout>
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </Layout>
     </SessionProvider>
   )
