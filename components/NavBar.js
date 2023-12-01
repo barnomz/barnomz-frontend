53656569e94072194a2e293ee5aca417ed31a0d7
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-12-01 23:12:50 +0330

    Remove priority tag from logo image in NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,7 +5,7 @@ import BBtn from '@/components/BBtn'
 export default function NavBar() {
   return (
     <div className="px-[1.5rem] py-[0.75rem] h-[3.75rem] w-full flex justify-between items-center bg-primary/50 backdrop-blur">
-      <Image src={logoSvg} alt="Barnomz Logo" priority />
+      <Image src={logoSvg} alt="Barnomz Logo" />
       <div className='flex gap-4'>
         <BBtn to='/auth/login' color='primary' className='h-[2.25rem]'>
           ورود
