24e0e868a319155ab1e2dbdda96a4ad1549e7c57
mhdsdt <mahdi.saadatbakht.1381@gmail.com>
2023-12-01 22:55:15 +0330

    Add title tag and a button in login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -5,6 +5,9 @@ import BForm from '@/components/BForm'
 import BInput from '@/components/BInput'
 import BInputPassword from '@/components/BInputPassword'
 import BBtn from '@/components/BBtn'
+import Head from 'next/head'
+import BLink from '@/components/BLink'
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 
 export default function Login() {
   const [credentials, setCredentials] = useState({ username: '', password: '' })
@@ -33,28 +36,36 @@ export default function Login() {
   }
 
   return (
-    <div className='h-full bg-dark-blue flex justify-center items-center'>
-      <div className='w-full max-w-md p-8 space-y-8 bg-light-blue rounded-xl'>
-        <h2 className='text-2xl text-grey-50 font-bold'>ورود به برنومز</h2>
-        <BForm onSubmit={handleLogin}>
-          <BInput
-            label='نام کاربری'
-            placeholder='نام کاربری خود را وارد نمایید'
-            value={credentials.username}
-            onChange={updateField('username')}
-          />
-          <BInputPassword
-            label='رمز عبور'
-            placeholder='رمز عبور خود را وارد نمایید'
-            value={credentials.password}
-            onChange={updateField('password')}
-          />
-          <BBtn type='submit' block>
-            ورود
-          </BBtn>
-        </BForm>
+    <>
+      <Head>
+        <title>برنومز - ورود</title>
+      </Head>
+      <div className='h-full bg-dark-blue flex justify-center items-center'>
+        <div className='bg-primary/50 backdrop-blur w-full max-w-md p-8 space-y-8 bg-light-blue rounded-xl'>
+          <h2 className='text-2xl text-grey-50 font-bold'>ورود به برنومز</h2>
+          <BForm onSubmit={handleLogin}>
+            <BInput
+              label='نام کاربری'
+              placeholder='نام کاربری خود را وارد نمایید'
+              value={credentials.username}
+              onChange={updateField('username')}
+            />
+            <BInputPassword
+              label='رمز عبور'
+              placeholder='رمز عبور خود را وارد نمایید'
+              value={credentials.password}
+              onChange={updateField('password')}
+            />
+            <BBtn type='submit' className='mb-4' block>
+              ورود
+            </BBtn>
+            <BLink to='/auth/register' postIcon={faArrowLeft} iconSize='1x'>
+              ثبت‌نام نکرده‌اید؟
+            </BLink>
+          </BForm>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
