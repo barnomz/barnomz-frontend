5779aa267220717f5e2a6acc36e75bd40c02eb4e
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-12-17 07:58:54 +0330

    Add auth middleware for schedule page

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,6 +1,23 @@
+import { getSession } from 'next-auth/react'
+
 export default function SchedulePage() {
-  return (
-    <>
-    </>
-  )
+  return <></>
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context
+  const session = await getSession({ req })
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { session },
+  }
 }
