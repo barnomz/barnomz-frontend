6e309f48db695b7c1bb6c44a924ba8b161942e37
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-12-18 15:01:31 +0330

    Move consts to const.js file

---
+++ constants/const.js
@@ -0,0 +1,134 @@
+export const persianNumbers = [
+  /۰/g,
+  /۱/g,
+  /۲/g,
+  /۳/g,
+  /۴/g,
+  /۵/g,
+  /۶/g,
+  /۷/g,
+  /۸/g,
+  /۹/g,
+]
+
+export const arabicNumbers = [
+  /٠/g,
+  /١/g,
+  /٢/g,
+  /٣/g,
+  /٤/g,
+  /٥/g,
+  /٦/g,
+  /٧/g,
+  /٨/g,
+  /٩/g,
+]
+
+export const iPhoneNumbers = [
+  /٠/g,
+  /١/g,
+  /٢/g,
+  /٣/g,
+  /۴/g,
+  /۵/g,
+  /۶/g,
+  /٧/g,
+  /٨/g,
+  /٩/g,
+]
+export const englishNumbers = [
+  /0/g,
+  /1/g,
+  /2/g,
+  /3/g,
+  /4/g,
+  /5/g,
+  /6/g,
+  /7/g,
+  /8/g,
+  /9/g,
+]
+
+export const weekDays = [
+  'شنبه',
+  'یکشنبه',
+  'دوشنبه',
+  'سه‌شنبه',
+  'چهارشنبه',
+  'پنج‌شنبه',
+]
+
+export const tabs = [
+  {
+    id: 1,
+    title: 'برنامه ۱',
+    courses: [
+      {
+        id: 1,
+        code: 'CSE101',
+        title: 'برنامه‌سازی وب',
+        lecturer: 'محمدرضا محمدی',
+        daysOfWeek: [1, 3],
+        startTime: '13:30',
+        endTime: '15:00',
+      },
+      {
+        id: 2,
+        code: 'CSE102',
+        title: 'یادگیری ماشین',
+        lecturer: 'ابولفضل مطهری',
+        daysOfWeek: [2, 4],
+        startTime: '09:00',
+        endTime: '10:30',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'برنامه ۲',
+    courses: [
+      {
+        id: 1,
+        code: 'CSE101',
+        title: 'برنامه‌سازی وب',
+        lecturer: 'محمدرضا محمدی',
+        daysOfWeek: [1, 3],
+        startTime: '13:30',
+        endTime: '15:00',
+      },
+      {
+        id: 2,
+        code: 'CSE102',
+        title: 'یادگیری ماشین',
+        lecturer: 'ابولفضل مطهری',
+        daysOfWeek: [2, 4],
+        startTime: '09:00',
+        endTime: '10:30',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'برنامه ۳',
+    courses: [
+      {
+        id: 1,
+        code: 'CSE101',
+        title: 'برنامه‌سازی وب',
+        lecturer: 'محمدرضا محمدی',
+        daysOfWeek: [1, 3],
+        startTime: '13:30',
+        endTime: '15:00',
+      },
+      {
+        id: 2,
+        code: 'CSE102',
+        title: 'یادگیری ماشین',
+        lecturer: 'ابولفضل مطهری',
+        daysOfWeek: [2, 4],
+        startTime: '09:00',
+        endTime: '10:30',
+      },
+    ],
+  },
+]
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,40 +1,6 @@
+import { persianNumbers, arabicNumbers, iPhoneNumbers } from '@/constants/const'
+
 export function convertPersianNumberToEnglish(number) {
-  const persianNumbers = [
-    /۰/g,
-    /۱/g,
-    /۲/g,
-    /۳/g,
-    /۴/g,
-    /۵/g,
-    /۶/g,
-    /۷/g,
-    /۸/g,
-    /۹/g,
-  ]
-  const arabicNumbers = [
-    /٠/g,
-    /١/g,
-    /٢/g,
-    /٣/g,
-    /٤/g,
-    /٥/g,
-    /٦/g,
-    /٧/g,
-    /٨/g,
-    /٩/g,
-  ]
-  const iPhoneNumbers = [
-    /٠/g,
-    /١/g,
-    /٢/g,
-    /٣/g,
-    /۴/g,
-    /۵/g,
-    /۶/g,
-    /٧/g,
-    /٨/g,
-    /٩/g,
-  ]
   for (let i = 0; i < 10; i++) {
     number = number
       .replace(persianNumbers[i], i)
