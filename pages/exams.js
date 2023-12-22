0d94fa7e04d3084f6a422298f83a41af821b7dff
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-12-22 21:11:25 +0330

    Add max width for container of exams page

--- pages/exams.js
+++ pages/exams.js
@@ -9,7 +9,7 @@ export default function ExamsPage() {
         <title>برنومز | برنامه امتحانات</title>
       </Head>
       <div className='aboslute flex h-full w-full justify-center p-6 text-white'>
-        <div className='flex grow flex-col justify-between gap-4 rounded-xl bg-primary/50 p-4 backdrop-blur'>
+        <div className='flex max-w-[98.875rem] grow flex-col justify-between gap-4 rounded-xl bg-primary/50 p-4 backdrop-blur'>
           <h1 className='text-2xl font-bold'>برنامه امتحانات</h1>
           <ScheduleTabs />
           <ExamsTable />
