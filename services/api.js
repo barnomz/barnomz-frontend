8bf822102bb3586b16622aeef062fd675800d163
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-18 22:12:47 +0330

    fix: Add lecturer api service

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import auth from './api/auth'
 import schedule from './api/schedule'
+import lecturer from './api/lecturer'
 
 const api = {
   auth,
   schedule,
+  lecturer,
 }
 
 export default api
---
+++ services/api/lecturer.js
@@ -0,0 +1,23 @@
+import dataFetcher from '@/services/dataFetcher'
+
+const NAMESPACE = '/schedules/'
+
+const schedule = {
+  fetchLecturer(config) {
+    return dataFetcher.get(NAMESPACE + config.lecturerId + '/', config)
+  },
+  fetchLecturerReviews(config) {
+    return dataFetcher.get(NAMESPACE + config.lecturerId + '/reviews/', config)
+  },
+  addReview(config) {
+    return dataFetcher.post(NAMESPACE + config.lecturerId + '/reviews/', config)
+  },
+  deleteReview(config) {
+    return dataFetcher.delete(
+      NAMESPACE + config.lecturerId + '/reviews/',
+      config,
+    )
+  },
+}
+
+export default schedule
