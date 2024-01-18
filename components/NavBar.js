e7cbd67107c6968d7da310ed2abb6472195416ab
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-18 22:12:22 +0330

    fix: Make logo in NavBar clickable

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,6 +3,7 @@ import logoSvg from '@/public/images/barnomz-horizontal-logo.svg'
 import BBtn from '@/components/dls/BBtn'
 import { signOut, useSession } from 'next-auth/react'
 import NavBarMenu from '@/components/NavBarMenu'
+import Link from 'next/link'
 
 export default function NavBar() {
   // const { session } = useSession()
@@ -31,7 +32,9 @@ export default function NavBar() {
 
   return (
     <div className='sticky flex h-[3.75rem] w-full items-center justify-between bg-primary/50 px-[1.5rem] py-[0.75rem] backdrop-blur'>
-      <Image src={logoSvg} alt='Barnomz Logo' priority />
+      <Link href={'/'}>
+        <Image src={logoSvg} alt='Barnomz Logo' priority />
+      </Link>
       {session && <NavBarMenu />}
       {session ? navBarEndLoggedIn : navBarEndLoggedOut}
     </div>
