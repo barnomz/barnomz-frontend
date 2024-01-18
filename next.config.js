c7a0446007a55ed4f2bdbd696e43f0dbc0bf3cf8
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-18 22:37:54 +0330

    fix: Add redirect for lecturers page in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/lecturers',
+        destination: '/lecturers/1',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
