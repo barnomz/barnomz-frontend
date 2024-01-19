f86bca45bc56bfb2ba488cdf550c6973a615d406
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-19 17:41:30 +0330

    fix: Add callbacks to [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,20 +7,48 @@ export default NextAuth({
     CredentialsProvider({
       credentials: {
         username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' }
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        await api.auth.login({ data: credentials }).then((response) => {
-          return Promise.resolve(response.data)
-        }).catch((error) => {
-          console.error(error)
-          return Promise.resolve(null)
-        })
-      }
-    })
+        await api.auth
+          .login({ data: credentials })
+          .then((response) => {
+            return Promise.resolve(response.data)
+          })
+          .catch((error) => {
+            console.error(error)
+            return Promise.resolve(null)
+          })
+      },
+    }),
   ],
   pages: {
     signIn: '/auth/login',
-    signOut: '/auth/logout'
-  }
+    signOut: '/auth/logout',
+  },
+  callbacks: {
+    jwt: async (token, user) => {
+      if (user) {
+        const { token, user: u } = user
+
+        return {
+          token,
+          user: u,
+        }
+      }
+
+      // if (Date.now() < new Date(token.accessTokenExpiresAt).getTime()) {
+      //   return token
+      // }
+
+      return null
+    },
+    session: (session, token) => {
+      session.user = token.user
+      session.accessToken = token.token
+      session.error = token.error
+
+      return session
+    },
+  },
 })
