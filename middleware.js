e7de2bb4c229578c83eed228264e9689188f28c6
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-19 17:54:36 +0330

    fix: Comment /schedules and /exams as protected pages until authentication is ready in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/exams', '/schedule'] }
+// export const config = { matcher: ['/exams', '/schedules'] }
+export const config = { matcher: [] }
