dc62bc88ee6b07cdefad78a9cd54d24d8eb7c930
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-27 22:25:25 +0330

    fix: Add messages js file to constants dir

---
+++ constants/messages.js
@@ -0,0 +1,8 @@
+const messages = {
+  ERROR_OCCURRED: 'خطایی رخ داده است',
+  REGISTER_SUCCESS: 'ثبت‌نام شما موفقیت‌آمیز بود',
+  LOGIN_SUCCESS: 'ورود شما موفقیت‌آمیز بود',
+  FORM_INVALID: 'فیلدها را به درستی مقداردهی کنید',
+}
+
+export default messages
