ff6ccb279db2cbf27d04ac273c5862e3c49304dd
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-27 23:38:40 +0330

    fix: Add div for removing cls from Schedules component

--- components/schedule/Schedule.js
+++ components/schedule/Schedule.js
@@ -44,59 +44,61 @@ export default function SchedulePage() {
   }
 
   return (
-    <FullCalendar
-      plugins={[timeGridPlugin]}
-      initialView='timeGridWeek'
-      headerToolbar={false}
-      editable={false}
-      selectable={false}
-      selectMirror={false}
-      expandRows
-      dayMaxEvents
-      weekends
-      initialEvents={courses}
-      eventClick={handleEventClick}
-      eventsSet={handleEvents}
-      slotMinTime={'07:00'}
-      slotMaxTime={'20:00'}
-      direction='rtl'
-      initialDate={'2023-12-30'}
-      locale={faLocale}
-      firstDay={2}
-      height={'700px'}
-      views={{
-        timeGrid: {
-          component: DayTimeColsView,
-          usesMinMaxTime: true,
-          allDaySlot: false,
-          slotDuration: '01:00:00',
-          slotEventOverlap: true,
-          duration: { days: 6 },
-          slotLabelContent: ({ date }) => (
-            <div className='me-1 ms-2 text-sm font-medium'>
-              {convertPersianNumberToEnglish(date.getHours().toString())}
-            </div>
-          ),
-          dayHeaderContent: ({ date }) => (
-            <div className='pb-2 text-sm font-medium'>
-              {
-                weekDays[
-                  ((date.getDay() % weekDays.length) + weekDays.length) %
-                    weekDays.length
-                ]
-              }
-            </div>
-          ),
-          eventContent: (event) => (
-            <Course
-              course={{
-                title: event.event.title,
-                ...event.event.extendedProps,
-              }}
-            ></Course>
-          ),
-        },
-      }}
-    />
+    <div className='h-[700px]'>
+      <FullCalendar
+        plugins={[timeGridPlugin]}
+        initialView='timeGridWeek'
+        headerToolbar={false}
+        editable={false}
+        selectable={false}
+        selectMirror={false}
+        expandRows
+        dayMaxEvents
+        weekends
+        initialEvents={courses}
+        eventClick={handleEventClick}
+        eventsSet={handleEvents}
+        slotMinTime={'07:00'}
+        slotMaxTime={'20:00'}
+        direction='rtl'
+        initialDate={'2023-12-30'}
+        locale={faLocale}
+        firstDay={2}
+        height={'700px'}
+        views={{
+          timeGrid: {
+            component: DayTimeColsView,
+            usesMinMaxTime: true,
+            allDaySlot: false,
+            slotDuration: '01:00:00',
+            slotEventOverlap: true,
+            duration: { days: 6 },
+            slotLabelContent: ({ date }) => (
+              <div className='me-1 ms-2 text-sm font-medium'>
+                {convertPersianNumberToEnglish(date.getHours().toString())}
+              </div>
+            ),
+            dayHeaderContent: ({ date }) => (
+              <div className='pb-2 text-sm font-medium'>
+                {
+                  weekDays[
+                    ((date.getDay() % weekDays.length) + weekDays.length) %
+                      weekDays.length
+                  ]
+                }
+              </div>
+            ),
+            eventContent: (event) => (
+              <Course
+                course={{
+                  title: event.event.title,
+                  ...event.event.extendedProps,
+                }}
+              ></Course>
+            ),
+          },
+        }}
+      />
+    </div>
   )
 }
