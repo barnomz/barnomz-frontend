8f558636bbce52208f9af2d6647a621a8c9123f1
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-27 23:34:42 +0330

    fix: Add authorization header to axios

--- services/axios.js
+++ services/axios.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
+import { getSession } from 'next-auth/react'
 
 const baseUrl = 'http://127.0.0.1:8000/api'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+  },
+})
+
+axiosInstance.interceptors.request.use(async (request) => {
+  const session = await getSession()
+
+  if (session) {
+    request.headers = {
+      ...request.headers,
+      Authorization: `Bearer ${session.accessToken}`,
+    }
   }
+
+  return request
 })
 
 export default axiosInstance
