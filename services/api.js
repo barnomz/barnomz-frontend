0852c6698c208ef4a2260562cc01738faccbffae
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-28 00:39:56 +0330

    fix: Add department and comment api services to api

--- services/api.js
+++ services/api.js
@@ -1,11 +1,15 @@
 import auth from './api/auth'
 import schedule from './api/schedule'
 import lecturer from './api/lecturer'
+import department from './api/department'
+import comment from './api/comment'
 
 const api = {
   auth,
   schedule,
   lecturer,
+  department,
+  comment,
 }
 
 export default api
