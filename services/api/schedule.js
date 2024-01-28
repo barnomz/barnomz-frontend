11a4c60fbc28112eac55d7563c6191866f110711
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-28 11:55:55 +0330

    fix: Add slash at the end of the NAMESPACE of schedule api service

--- services/api/schedule.js
+++ services/api/schedule.js
@@ -1,18 +1,18 @@
 import dataFetcher from '@/services/dataFetcher'
 
-const NAMESPACE = '/schedules'
+const NAMESPACE = '/schedules/'
 
 const schedule = {
   fetchSchedules(config) {
-    return dataFetcher.get(NAMESPACE + '/', config)
+    return dataFetcher.get(NAMESPACE, config)
   },
   addSchedule(config) {
-    return dataFetcher.post(NAMESPACE + '/', config)
+    return dataFetcher.post(NAMESPACE, config)
   },
   deleteSchedule(config) {
-    return dataFetcher.delete(NAMESPACE + '/', config)
+    return dataFetcher.delete(NAMESPACE + config.scheduleId + '/', config)
   },
-  addCourse(config) {
+  addCourseToSchedule(config) {
     return dataFetcher.post(NAMESPACE + config.scheduleId + '/course/', config)
   },
   deleteCourse(config) {
