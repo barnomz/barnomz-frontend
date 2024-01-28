0e5b43d3c1b9a7019ea3409fc037ab612d0c2e91
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2024-01-28 11:55:06 +0330

    fix: Add slash at the end of the NAMESPACE of department api service

--- services/api/department.js
+++ services/api/department.js
@@ -1,10 +1,10 @@
 import dataFetcher from '@/services/dataFetcher'
 
-const NAMESPACE = '/department'
+const NAMESPACE = '/departments/'
 
 const department = {
   fetchColleges(config) {
-    return dataFetcher.get(NAMESPACE + '/', config)
+    return dataFetcher.get(NAMESPACE, config)
   },
   fecthCollegeCourses(config) {
     return dataFetcher.get(NAMESPACE + config.collegeId + '/course/', config)
